7e3abbd0342d093444954f1ed7922ca5d1a2657d
Elain007 <liuyh022272@163.com>
2025-02-03 11:24:18 +0000

    update

--- docs/P5_Painting/sketch.js
+++ docs/P5_Painting/sketch.js
@@ -1,28 +1,38 @@
-let bearColor = [139, 90, 43]; // 默认棕色
+let bearColor;
+let colors;
+let colorIndex = 0;
 
 function setup() {
   createCanvas(400, 400);
+  colors = [
+    [139, 90, 43],   // default brown
+    [255, 255, 255], // white
+    [101, 67, 33],   // dark brown
+    [169, 169, 169]  // gray
+  ];
+  bearColor = colors[colorIndex];
+  setInterval(changeColor, 5000); // change color every 5 seconds
 }
 
 function draw() {
   background(220);
   
-  // 头部
+  // head
   fill(bearColor);
   noStroke();
   ellipse(200, 200, 180, 160);
   
-  // 耳朵
+  // ear
   fill(bearColor);
   ellipse(140, 130, 60, 60);
   ellipse(260, 130, 60, 60);
   
-  // 耳朵内侧
+  // inside ear
   fill([bearColor[0] + 42, bearColor[1] + 11, bearColor[2] - 14]);
   ellipse(140, 130, 40, 40);
   ellipse(260, 130, 40, 40);
   
-  // 眼睛
+  // eyes
   fill(255);
   ellipse(170, 180, 35, 35);
   ellipse(230, 180, 35, 35);
@@ -31,25 +41,19 @@ function draw() {
   ellipse(170, 180, 15, 15);
   ellipse(230, 180, 15, 15);
   
-  // 鼻子
+  // nose
   fill(0);
   ellipse(200, 210, 20, 15);
   
-  // 嘴巴
+  // mouth
   stroke(0);
   strokeWeight(3);
   noFill();
   arc(200, 220, 30, 20, 0, PI);
 }
 
-function keyPressed() {
-  if (key === 'W') {
-    bearColor = [255, 255, 255]; // 白色
-  } else if (key === 'B') {
-    bearColor = [101, 67, 33]; // 深棕色
-  } else if (key === 'G') {
-    bearColor = [169, 169, 169]; // 灰色
-  }
+function changeColor() {
+  colorIndex = (colorIndex + 1) % colors.length;
+  bearColor = colors[colorIndex];
 }
-
 
