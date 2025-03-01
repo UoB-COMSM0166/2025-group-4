5fad77fbe32ba787ce599276da9cd697425ded60
Elain007 <liuyh022272@163.com>
2025-03-01 14:25:27 +0000

    uodate

--- docs/src/preload.js
+++ docs/src/preload.js
@@ -1,9 +1,9 @@
 // src/preload.js
 export function preload() {
   window.playerImages = [];
-  window.playerImages.push(loadImage('arts resources/7.png'));
-  window.playerImages.push(loadImage('arts resources/8.png'));
-  window.playerImages.push(loadImage('arts resources/9.png'));
+  window.playerImages.push(loadImage('images/7.png'));
+  window.playerImages.push(loadImage('images/8.png'));
+  window.playerImages.push(loadImage('images/9.png'));
   // 其它资源加载……
 }
 window.preload = preload;
