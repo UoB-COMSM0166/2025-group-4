da3a52bd02ba0aa0509dd15fc192b6b45779a747
Elain007 <liuyh022272@163.com>
2025-03-01 01:18:41 +0000

    create and upload

---
+++ docs/src/preload.js
@@ -0,0 +1,8 @@
+// preload.js
+let playerImages = [];  // 用于存放主角图片
+
+function preload() {
+  playerImages.push(loadImage('assets/7.png'));
+  playerImages.push(loadImage('assets/8.png'));
+  playerImages.push(loadImage('assets/9.png'));
+}
